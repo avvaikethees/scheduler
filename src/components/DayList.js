a49525f32a8700feb1f4a7a919556531f4dc1052
Avvai Ketheeswaran <avvaik@Avvais-MacBook-Pro.local>
2020-10-27 14:50:03 -0700

    Built Interviewer List Item component and stories

--- src/components/DayList.js
+++ src/components/DayList.js
@@ -2,7 +2,7 @@ import React from "react";
 import DayListItem from "./DayListItem";
 
 export default function DayList(props) {
-  const days = props.days.map(day => {
+  const daysItems = props.days.map(day => {
     return (
       <DayListItem
         key={day.id}
@@ -16,6 +16,6 @@ export default function DayList(props) {
 
 
   return (
-  <ul>{days}</ul>
+  <ul>{daysItems}</ul>
   );
 }
