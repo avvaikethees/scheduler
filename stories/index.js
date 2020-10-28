b76d7d00bb1c219047ce6ab4b28acffacb75ac2c
Avvai Ketheeswaran <avvaik@Avvais-MacBook-Pro.local>
2020-10-27 23:03:16 -0700

    Built the Appointment component with it's children components except for the Form

--- stories/index.js
+++ stories/index.js
@@ -8,8 +8,15 @@ import "index.scss";
 import Button from "components/Button";
 import DayListItem from "components/DayListItem";
 import DayList from "components/DayList";
-import InterviewerListItem from "components/InterviewerListItem"
-
+import InterviewerListItem from "components/InterviewerListItem";
+import InterviewerList from "components/InterviewerList";
+import Appointment from "components/Appointment"
+import Header from "components/Appointment/Header.js";
+import Empty from "components/Appointment/Empty.js";
+import Show from "components/Appointment/Show.js";
+import Confirm from "components/Appointment/Confirm.js";
+import Status from "components/Appointment/Status.js";
+import Error from "components/Appointment/Error.js";
 storiesOf("Button", module)
   .addParameters({
     backgrounds: [{ name: "dark", value: "#222f3e", default: true }]
@@ -97,6 +104,68 @@ storiesOf("Button", module)
           id={interviewer.id}
           name={interviewer.name}
           avatar={interviewer.avatar}
-          setInterviewer={action("setInterviewer")}
+          setInterviewer={event => action("setInterviewer")(interviewer.id)}
         />
       ));
+
+      const interviewers = [
+        { id: 1, name: "Sylvia Palmer", avatar: "https://i.imgur.com/LpaY82x.png" },
+        { id: 2, name: "Tori Malcolm", avatar: "https://i.imgur.com/Nmx0Qxo.png" },
+        { id: 3, name: "Mildred Nazir", avatar: "https://i.imgur.com/T2WwVfS.png" },
+        { id: 4, name: "Cohana Roy", avatar: "https://i.imgur.com/FK8V841.jpg" },
+        { id: 5, name: "Sven Jones", avatar: "https://i.imgur.com/twYrpay.jpg" }
+      ];
+      
+      storiesOf("InterviewerList", module)
+        .addParameters({
+          backgrounds: [{ name: "dark", value: "#222f3e", default: true }]
+        })
+        .add("Initial", () => (
+          <InterviewerList
+            interviewers={interviewers}
+            setInterviewer={action("setInterviewer")}
+          />
+        ))
+        .add("Preselected", () => (
+          <InterviewerList
+            interviewers={interviewers}
+            interviewer={3}
+            setInterviewer={action("setInterviewer")}
+          />
+        ));
+
+
+        storiesOf("Appointment", module)
+          .addParameters({
+            backgrounds: [{ name: "white", value: "#fff", default: true }]
+          })
+          .add("Appointment with Time", () => <Appointment />)
+          .add("Time", () => <Appointment time="12pm" />)
+          .add("Header", () => <Header time = "12pm" />)
+          .add("Empty", () => <Empty  onAdd={action("onAdd")} />)
+          .add("Show", () => (
+            <Show 
+                student="Lydia Miller-Jones"
+                interviewer={interviewer}
+                onEdit={action("onEdit")}
+                onDelete={action("onDelete")}
+            />
+          ))
+          .add("Confirm", () => (
+            <Confirm
+              message="Delete the appointment?"
+              onConfirm={action("onConfirm")}
+              onCancel={action("onCancel")}
+            />
+          ))
+          .add("Status", () => (
+            <Status
+              message="Deleting"
+            />
+          ))
+          .add("Error", () => (
+            <Error
+              message="Could not delete appointment."
+              onClose={action("onClose")}
+            />
+          ));
