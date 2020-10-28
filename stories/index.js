3026f236af9420cbfcad65b9c3170a78316fb7bb
Avvai Ketheeswaran <avvaik@Avvais-MacBook-Pro.local>
2020-10-28 09:22:20 -0700

    Built the Form component (without state)

--- stories/index.js
+++ stories/index.js
@@ -17,7 +17,7 @@ import Show from "components/Appointment/Show.js";
 import Confirm from "components/Appointment/Confirm.js";
 import Status from "components/Appointment/Status.js";
 import Error from "components/Appointment/Error.js";
-
+import Form from "components/Appointment/Form.js";
 
 storiesOf("Button", module)
   .addParameters({
@@ -169,5 +169,21 @@ storiesOf("Appointment", module)
     <Error
       message="Could not delete appointment."
       onClose={action("onClose")}
+    />
+  ))
+  .add("Form Edit", () => (
+    <Form
+      name="Lighthouse Lab Student"
+      interviewers={interviewers}
+      interviewer={1}
+      onSave={action("onSave")}
+      onCancel={action("onCancel")}
+    />
+  ))
+  .add("Form Create", () => (
+    <Form 
+      interviewers={interviewers}
+      onSave={action("onSave")}
+      onCancel={action("onCancel")}
     />
   ));
