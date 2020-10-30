b27682bf55539a93860116cde371f8b5da54de8a
Avvai Ketheeswaran <avvaik@Avvais-MacBook-Pro.local>
2020-10-30 00:58:39 -0700

    started using useVisualMode hook to render appointment block to EMPTY, SHOW, and CREATE

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -3,23 +3,35 @@ import "./styles.scss";
 import Header from "./Header";
 import Show from "./Show";
 import Empty from "./Empty";
+import Form from "./Form";
+import useVisualMode from "hooks/useVisualMode";
 
 
 
 export default function Appointment(props) {
+  const EMPTY = "EMPTY";
+  const SHOW = "SHOW";
+  const CREATE = "CREATE";
 
+  const { mode, transition, back } = useVisualMode (
+    props.interview ? SHOW : EMPTY
+  );
 
   return (
     <article className="appointment">
       <Header time={props.time}/>
-      {props.interview ?
-      <Show 
-        student= {props.interview.student}
-        interviewer= {props.interview.interviewer}
+      {mode === EMPTY && <Empty onAdd={ () => {console.log("Clicked onAdd"); transition(CREATE)} } />}
+      {mode === SHOW && (
+        <Show 
+          student= {props.interview.student}
+          interviewer= {props.interview.interviewer}
         />
-      :
-      <Empty />
-      }
+      )}
+      {mode === CREATE && (
+        <Form 
+          interviewers = {[]}
+          onCancel = {() => back()}
+        />)}
     </article>
   )
 }
